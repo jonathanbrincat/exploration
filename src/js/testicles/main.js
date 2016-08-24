8d48d933f943fbdaee9bb1e9cb0d75a3919be4e0
Jonathan Brincat <jonathan@pix8.co.uk>
2016-08-24 02:17:51 +0100

    some react refinements and jazz

--- src/js/testicles/main.js
+++ src/js/testicles/main.js
@@ -6,14 +6,33 @@ import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
 
+    constructor() {
+        super();
+
+        this.bar = "foobar";
+    }
+
+    foo() {
+        return "foobar";
+    }
+
     render() {
+        const name = "Jon";
+
         return (
             //JSX
-            <h1>React says Hi!</h1>
+            <div>
+                <h1>React says Hi!</h1>
+                <h2>JSX can only return one parent DOM element; ie. root node</h2>
+                <h3>Here is some inline javascript; whats 1 + 2? {1 + 2}</h3>
+                <h4>Heres another way; Hi my name is { name }</h4>
+                <h5>Heres another way; Hello { this.foo() }</h5>
+                <h5>Heres a better way; Hello { this.bar }</h5>
+            </div>
         );
     }
 }
 
-const app = document.getElementById("app");
+const appReact = document.getElementById("app-react");
 
-ReactDOM.render(<Layout />, app);
+ReactDOM.render(<Layout />, appReact);
