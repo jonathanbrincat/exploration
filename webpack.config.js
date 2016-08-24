7ca1cefdfe571711092d4ed425b719ba572c2820
Jonathan Brincat <jonathan@pix8.co.uk>
2016-08-24 01:37:41 +0100

    babel and react underpinnings

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,23 @@ module.exports = {
 	context: __dirname + "/src",
 	devtool: debug ? "inline-sourcemap" : null,
 	entry: "./js/testicles/main.js",
+
+	module: {
+		loaders: [
+
+			// Babel
+			{
+				test: /\.js?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: "babel-loader",
+				query: {
+					presets: ["react", "es2015", "stage-0"],
+					plugins: ["react-html-attrs", "transform-class-properties", "transform-decorators-legacy"]
+				}
+			}
+		]
+	},
+
 	output: {
 		//path: __dirname + "/src" + "/scripts",
 		path: "dist/scripts",
