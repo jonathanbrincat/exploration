81f0f455f16b06a21922bff14436884b7bd3d416
Jonathan Brincat <jonathan@pix8.co.uk>
2016-08-24 01:55:21 +0100

    added vue into the blend; basic react demo set

--- src/js/testicles/main.js
+++ src/js/testicles/main.js
@@ -1,2 +1,19 @@
 require("./module1.js");
 require("./module2.js");
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+
+    render() {
+        return (
+            //JSX
+            <h1>React says Hi!</h1>
+        );
+    }
+}
+
+const app = document.getElementById("app");
+
+ReactDOM.render(<Layout />, app);
